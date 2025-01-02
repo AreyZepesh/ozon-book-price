0fb0e74b464ef30f28b5421de2037589067b51db
aaz <arey_kz@mail.ru>
2025-01-02 11:57:02 +0500

    update sqlite.DB and bookClass: make addBook to DB

--- bookClass.py
+++ bookClass.py
@@ -1,45 +1,25 @@
-from sqliteDB import getISBNs, getArticles
+import sqliteDB
 
 class Book:
-    def __init__(self, title, author = "", year_start = "", year_end = "", **kwargs) -> None:
-        # TODO издатель - возвращать id если есть в базе
-        """Рекомендуемые переменные, которые взаимодействуют с БД / 
-        Recommended variables that interact with the database:
-        **kwargs: dict = {
-        'publisher': str(), 
-        'publisher_ozon_id': int(), 
-        'isbnS': list(), 
-        'articleS': list()}
-        
-        Эти данные будут вставлены в БД. Идентификатор книги возвращается из БД автоматически /
-        This data will be inserted to DB. Book's ID returned from DB automatically
-        """
-
+    def __init__(self, title, author = "", year_start = "", year_end = "") -> None:
+        # TODO функция издателя, исбн и артикля, получение из БД
         self.title: str = title
         self.author: str = author
         self.year_start: int = year_start
         self.year_end: int = year_end
+    
+    def __str__(self) -> str:
+        return f"{self.title}, {self.author}, {self.year_start}, {self.year_end}"
+    
+    def sendToDB(self):
+        sqliteDB.addBook((self.title, self.author, self.year_start, self.year_end))
+        self.id = sqliteDB.getBookID(self.title)
 
-        self._kwargs = kwargs
+def main():
+    # Вызов класса из его модуля может привести к ошибкам имени модуля. 
+    # Отсюда будет __main__.Book, а из других мест: bookClass.Book.
+    pass
 
-        # self.publisher: int = publisher 
-        # self.publisher_ozon_id: int = publisher_ozon_id 
 
-        # ['id', 'title', 'author', 'year_start', 'year_end', 'publisher', 'publisher_ozon_id', 'have_isbn', 'have_article']
-        # self.id: int = book_id
-        # if 'have_isbn' in self._kwargs.keys():
-        #     if self._kwargs['have_isbn'] > 0:
-        #         self.isbnS = getISBNs(self.id)
-        # else:
-        #     self.isbnS = []
-
-        # if 'have_article' in self._kwargs.keys():
-        #     if self._kwargs['have_article'] > 0:
-        #         self.articleS = getArticles(self.id)
-        # else:
-        #     self.articleS = []
-
-dick = dict(title = 'test', author = "author", year_start = "st", year_end = "end", publisher = "ast", publisher_ozon_id = "2025", have_isbn = 3, have_article = 2)
-a = Book(**dick)
-print(a.title)
-print(vars(a))
+if __name__  == '__main__':
+    main()
--- sqliteDB.py
+++ sqliteDB.py
@@ -1,7 +1,12 @@
 import sqlite3
 import itertools
 
-def createDB(dbname):
+DEFAULT_DB = "ozon-books2.db"
+
+# Создание пустой БД
+def createDB(dbname=DEFAULT_DB) -> None:
+    """Создание БД по умолчанию, посредством допфункци (вызов внутри)
+    происходит создание базовых таблиц и представлений"""
     connect = sqlite3.connect(dbname)
     cursor = connect.cursor()
     
@@ -24,11 +29,12 @@ def createDB(dbname):
         connect.commit()
         connect.close()
 
-def createTables(cursor):
+def createTables(cursor) -> None:
+    """Создание базовых таблиц"""
     # Таблица id издателей на озон, книги к ней цепляются
     cursor.execute("""CREATE TABLE IF NOT EXISTS publisherS
                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    publisher TEXT,
+                    publisher TEXT UNIQUE,
                     publisher_ozon_id INTEGER NOT NULL)
                     """)
     # Таблица с книгами: 
@@ -36,7 +42,7 @@ def createTables(cursor):
     # и нужно ли смотреть по более точным параметрам: isbn, артикль, продовец, издательство
     cursor.execute("""CREATE TABLE IF NOT EXISTS bookS
                     (id INTEGER PRIMARY KEY AUTOINCREMENT,
-                    title TEXT NOT NULL,
+                    title TEXT NOT NULL UNIQUE,
                     author TEXT,
                     year_start INTEGER,
                     year_end INTEGER,
@@ -80,7 +86,8 @@ def createTables(cursor):
                     seller_ozon_id INTEGER NOT NULL)
                     """)
 
-def createViews(cursor):
+def createViews(cursor) -> None:
+    """Создание базовых представлений"""
     cursor.execute("""CREATE VIEW IF NOT EXISTS books_view AS
                     SELECT bookS.id AS book_id, title, author, year_start, year_end, 
                     publisherS.publisher, 
@@ -100,36 +107,52 @@ def createViews(cursor):
                     LEFT JOIN books ON priceS.book_id = books.id
                     """)
 
-def getISBNs(id, dbname) -> list:
+
+# Добавление/получшение книг, isbn, артиклей, издателей
+def addBook(book, dbname=DEFAULT_DB) -> None:
+    """Добавление или обновление книги в БД"""
+    # TODO ? возврат данных книги в виде словаря (для обновления класса книги)
     connect = sqlite3.connect(dbname)
     cursor = connect.cursor()
-
-    cursor.execute("""
-                    SELECT isbn FROM isbnS
-                    WHERE book_id IN (?)
-                    """, (id,))
     
-    isbnS = [s[0] for s in cursor.fetchall()]
-    connect.close()
-    return isbnS
+    try:
+        cursor.execute('BEGIN')
+        if not isinstance(book, tuple) or (len(book) != 4):
+            raise ValueError("""ERROR: Книга должна быть кортежем:
+                             1. с количеством значений равным 4 (четыре);
+                             2. со значениями перечисленных параметров: [title, author, year_start, year_end].""")
+        id = getBookID(book[0])
+        if id == 0:
+            del id
+            cursor.execute("""INSERT INTO bookS 
+                        (title, author, year_start, year_end)
+                        VALUES (?, ?, ?, ?)
+                        """, book) 
+        else:
+            book = (id,) + book
+            cursor.execute("""REPLACE INTO bookS 
+                        (id, title, author, year_start, year_end)
+                        VALUES (?, ?, ?, ?, ?)
+                        """, book) 
+        
 
-def getArticles(id, dbname) -> list:
-    connect = sqlite3.connect(dbname)
-    cursor = connect.cursor()
+        cursor.execute('COMMIT')
 
-    cursor.execute("""
-                    SELECT article FROM articleS
-                    WHERE book_id IN (?)
-                    """, (id,))
-    articleS = [s[0] for s in cursor.fetchall()]
-    connect.close()
-    return articleS
+    except Exception as ex:
+        # TODO LOG
+        cursor.execute('ROLLBACK')
+        raise ex
+    
+    finally:
+        connect.commit()
+        connect.close()
 
-def getBooks(dbname) -> list:
+def getAllBooks(dbname=DEFAULT_DB) -> list:
+    """Получить список словарей всех книг в БД"""
     bookS = []
 
-    con = sqlite3.connect(dbname)
-    cursor = con.cursor()
+    connect = sqlite3.connect(dbname)
+    cursor = connect.cursor()
     cursor.execute("PRAGMA table_info('books_view')")
     keyList = [k[1] for k in cursor.fetchall()]
     cursor.execute('SELECT * FROM books_view')
@@ -152,45 +175,73 @@ def getBooks(dbname) -> list:
                 t_book['articleS'] = []
 
         bookS.append(t_book)
-
+    connect.close()
     return bookS
 
-# TODO добавление книги из класса
-def addBook(book, dbname):
+def getBookID(title, dbname=DEFAULT_DB) -> int:
+    """Возвращает ID книги по названию, либо 0, если книга не найдена.
+    Генерирует исключение, если id больше одного, хотя поиск не может вернуть больше одного id)"""
+    title = (title, )
     connect = sqlite3.connect(dbname)
     cursor = connect.cursor()
+    cursor.execute("""SELECT id FROM books_view
+                   WHERE title IN (?)""", title)
+    res = cursor.fetchall()
+    connect.close()
+    if len(res) == 1:
+        id = res[0][0]
+    elif len(res) == 0:
+        id = 0
+    else:
+        # TODO log... а нужно ли это исключение?
+        raise ValueError("ERROR: Внезапно поиск ID вернул не одно или ноль значений")
+    return id
+
+def addISBN(id, dbname=DEFAULT_DB):
+    pass
+
+def getISBNs(id, dbname=DEFAULT_DB) -> list:
+    """Получить список isbn книги по id"""
+    connect = sqlite3.connect(dbname)
+    cursor = connect.cursor()
+
+    cursor.execute("""
+                    SELECT isbn FROM isbnS
+                    WHERE book_id IN (?)
+                    """, (id,))
     
-    try:
-        cursor.execute('BEGIN')
-        book_dict = {}
-        cursor.execute("""INSERT INTO bookS (title, author, year_start, year_end, publisher_id)
-                    VALUES ?
-                    """, book_dict)
+    isbnS = [s[0] for s in cursor.fetchall()]
+    connect.close()
+    return isbnS
 
-        cursor.execute('COMMIT')
+def addPublisher(publisher, publisher_id, dbname=DEFAULT_DB):
+    pass
 
-    except Exception as ex:
-        # TODO LOG
-        cursor.execute('ROLLBACK')
-        raise ex
+def getPublisher(publisher, dbname=DEFAULT_DB):
+    pass
+
+def getAllPublishers(dbname=DEFAULT_DB):
+    pass
+
+def addArticles(id, dbname=DEFAULT_DB):
+    pass
     
-    finally:
-        connect.commit()
-        connect.close()
+def getArticles(id, dbname=DEFAULT_DB) -> list:
+    """Получить список артиклей книги по id"""
+    connect = sqlite3.connect(dbname)
+    cursor = connect.cursor()
 
-def addPublisher(publisher, publisher_id, dbname):
-    pass
+    cursor.execute("""
+                    SELECT article FROM articleS
+                    WHERE book_id IN (?)
+                    """, (id,))
+    articleS = [s[0] for s in cursor.fetchall()]
+    connect.close()
+    return articleS
 
-def addISBN(id, dbname):
-    pass
-
-def addArticles(id, dbname):
-    pass
-
-def getBookID(title, dbname):
-    pass
-
+# Тестовые данные для БД. Вызывается в createDB
 def insertDataTest(cursor):
+        """Добавление данных для тестов"""
         cursor.execute("""INSERT INTO isbnS (isbn,book_id,id)
                     VALUES ('966-696-368-X',1,1),
                     ('978-5-17-042767-3',1,2),
@@ -205,7 +256,7 @@ def insertDataTest(cursor):
 def main():
     dbname = "ozon-books2.db"
     # createDB(dbname)
-    bookS = getBooks(dbname)
+    bookS = getAllBooks(dbname)
     
     print()
     for book in bookS:
